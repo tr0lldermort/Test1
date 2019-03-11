0f788c711185a49364fe26ce1ef23a0223e35055
tr0lldermort <dtbeckett@gmail.com>
2019-03-11 20:54:13 +0000

    test commit 1

---
+++ src/Test1.cpp
@@ -0,0 +1,24 @@
+#include "application.h"
+#line 1 "/home/danbot/Documents/ParticleCode/Test1/src/Test1.ino"
+/*
+ * Project Test1
+ * Description:
+ * Author:
+ * Date:
+ */
+
+// setup() runs once, when the device is first turned on.
+void setup();
+void loop();
+#line 9 "/home/danbot/Documents/ParticleCode/Test1/src/Test1.ino"
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+  Serial.begin(115200);
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+  Serial.println("fackorf 8===D--....__");
+  delay(1000);
+}
---
+++ src/Test1.ino
@@ -0,0 +1,19 @@
+/*
+ * Project Test1
+ * Description:
+ * Author:
+ * Date:
+ */
+
+// setup() runs once, when the device is first turned on.
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+  Serial.begin(115200);
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+  Serial.println("fackorf 8===D--....__");
+  delay(1000);
+}
