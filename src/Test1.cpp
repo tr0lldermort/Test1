3bafd7b271a23e9b6d68ce0e88a85b44b856af2b
tr0lldermort <dtbeckett@gmail.com>
2019-03-11 20:58:16 +0000

    cleaning up project and adding stars to penis

--- src/Test1.cpp
+++ src/Test1.cpp
@@ -19,6 +19,6 @@ void setup() {
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
   // The core of your code will likely live here.
-  Serial.println("fackorf 8===D--....__");
+  Serial.println("fackorf 8===D--....__***");
   delay(1000);
 }
